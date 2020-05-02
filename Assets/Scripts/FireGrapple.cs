390b7ded9e2c2bbda626a2cbce1f7fff70a76dc0
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2020-05-02 18:57:02 -0400

    Add raycast to check if web collides with anything on the "Ground" layer
    TODO: Change from raycast to projectile, to make web have travel time.

--- Assets/Scripts/FireGrapple.cs
+++ Assets/Scripts/FireGrapple.cs
@@ -6,20 +6,38 @@ public class FireGrapple : MonoBehaviour
 {
     [SerializeField]
     private GameObject grapplePrefab;
+    [SerializeField]
+    private float speedOfGrapple;
+    [SerializeField]
+    private float maxDistanceOfGrapple;
 
     private GameObject currentGrapple;
+    private GameObject player;
+    private RaycastHit2D raycastHit;
     private bool isWebActive;
 
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             if (isWebActive == false)
             {
+                int layerMask = LayerMask.GetMask("Ground");
                 Vector2 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                currentGrapple = Instantiate(grapplePrefab, transform.position, Quaternion.identity);
-                currentGrapple.GetComponent<Web>().SetEndPosition(clickedPosition);
-                isWebActive = true;
+                raycastHit = Physics2D.Raycast(player.transform.position, clickedPosition - (Vector2)player.transform.position, maxDistanceOfGrapple,layerMask);
+                if(raycastHit.collider!=null)
+                {
+                    Vector2 endPosition = raycastHit.point;
+                    currentGrapple = Instantiate(grapplePrefab, endPosition, Quaternion.identity);
+                    currentGrapple.GetComponent<Rigidbody2D>().AddForce(endPosition * speedOfGrapple);
+                    currentGrapple.GetComponent<Web>().SetEndPosition(endPosition);
+                    isWebActive = true;
+                }
             }
             else
             {
