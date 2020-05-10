831f68fe6df0832e27f9318f44d37191ac5f3da0
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2020-05-09 20:40:54 -0400

    Work on map, Add sliding layer that prevents movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     private float jumpHeight;
 
     private bool isGrounded=false;
+    private bool isSliding = false;
     private Rigidbody2D rb2d;
     private float moveHorizontal;
     private float moveVertical;
@@ -33,49 +34,63 @@ public class PlayerMovement : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
         //Check if player is grounded
-        int layerMask = LayerMask.GetMask("Ground");
+        int layerMask = LayerMask.GetMask("Ground","Water");
         ContactFilter2D filter = new ContactFilter2D();
         filter.SetLayerMask(layerMask);
         List<Collider2D> results = new List<Collider2D>();
         groundCheck.OverlapCollider(filter, results);
+        
         if (results.Count > 0)
         {
-            isGrounded = true;
+            if (results[0].gameObject.layer == 4) //Check if it overlaps the "Water" layer
+            {
+                isSliding = true; //If so, disable movement
+            }
+            else
+            {
+                isGrounded = true;
+                isSliding = false;
+            }
         }
         else
         {
             isGrounded = false;
+            isSliding = false;
         }
         if(isGrounded==true&&Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
+        Debug.Log(results[0]);
     }
 
     void FixedUpdate()
     {
-        if(isGrounded==true)
+        if (isSliding == false)
         {
-            Vector2 movement = new Vector2(moveHorizontal, 0);
-            rb2d.AddForce(movement * speed);
-            rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
-        }
-        else if(GetComponent<FireGrapple>().IsWebActive()) //If the player is not grounded but attached to the web
-        {
-            Vector2 movement = new Vector2(moveHorizontal, 0);
-            rb2d.AddForce((movement * speed)/swingControlModifier);
-            rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
-        }
-        else //If the player is in midair
-        {
-            Vector2 movement = new Vector2(moveHorizontal, 0);
-            rb2d.AddForce((movement * speed)/airControlModifier);
-            rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
-        }
+            if (isGrounded == true)
+            {
+                Vector2 movement = new Vector2(moveHorizontal, 0);
+                rb2d.AddForce(movement * speed);
+                rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
+            }
+            else if (GetComponent<FireGrapple>().IsWebActive()) //If the player is not grounded but attached to the web
+            {
+                Vector2 movement = new Vector2(moveHorizontal, 0);
+                rb2d.AddForce((movement * speed) / swingControlModifier);
+                rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
+            }
+            else //If the player is in midair
+            {
+                Vector2 movement = new Vector2(moveHorizontal, 0);
+                rb2d.AddForce((movement * speed) / airControlModifier);
+                rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
+            }
 
-        if (moveVertical != 0)
-        {
-            GetComponent<FireGrapple>().CallChangePositionOnWeb(moveVertical);
+            if (moveVertical != 0)
+            {
+                GetComponent<FireGrapple>().CallChangePositionOnWeb(moveVertical);
+            }
         }
     }
 
